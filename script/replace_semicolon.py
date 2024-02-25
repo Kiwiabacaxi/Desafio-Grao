f1761bc4cebf2b08df3d34903b09f754bd1c4298
Carlos <101848698+Kiwiabacaxi@users.noreply.github.com>
2024-02-25 18:12:33 -0300

    Tidying up the repository

--- script/replace_semicolon.py
+++ script/replace_semicolon.py
@@ -1,17 +1,11 @@
-# path
 path = './data/commerce_dataset.csv'
 
 # Abra o arquivo em modo de leitura
-# with open('seu_arquivo.csv', 'r') as file:
 with open(path, 'r') as file:
-    # Leia o conteúdo do arquivo
     file_data = file.read()
 
-# Substitua todos os ';' por ','
 file_data = file_data.replace(';', ',')
 
 # Abra o arquivo em modo de gravação
-# with open('seu_arquivo.csv', 'w') as file:
 with open(path, 'w') as file:
-    # Escreva o novo conteúdo no arquivo
     file.write(file_data)
